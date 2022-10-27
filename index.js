7357ae60688dd634fe13ed06803410160c41b0fe
sakir101 <sakirhossainf99@gmail.com>
2022-10-27 19:33:40 +0600

    data change

--- index.js
+++ index.js
@@ -3,11 +3,25 @@ const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 
+const category = require('./data/category.json');
+
 app.use(cors())
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
+app.get('/category', (req, res) => {
+    res.send(category);
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = category.find(c => c.id === id);
+    res.send(selectedCourse);
+})
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
